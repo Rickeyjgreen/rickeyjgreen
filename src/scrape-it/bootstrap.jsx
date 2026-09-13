6a28ac2ae39c09f3d0339a16469d8bc3e3af9c7b
Rickeyjgreen <rickeyj@actionshots.online>
2026-09-12 21:28:27 -0400

    Mount searchable vehicle inventory panel

--- src/scrape-it/bootstrap.jsx
+++ src/scrape-it/bootstrap.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import ScrapeItApp from './ScrapeItApp.jsx';
+import VehicleInventoryPanel from './VehicleInventoryPanel.jsx';
 import './scrape-it.css';
 
-createRoot(document.getElementById('root')).render(<ScrapeItApp />);
+createRoot(document.getElementById('root')).render(<><ScrapeItApp /><div className="scrape-it-shell"><main className="si-main"><VehicleInventoryPanel /></main></div></>);
