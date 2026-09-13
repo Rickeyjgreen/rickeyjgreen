8a1701d92b088b9f0bf609c7c40f2e774e26c381
Rickeyjgreen <rickeyj@actionshots.online>
2026-09-13 05:52:50 -0400

    Load control center styles

--- src/scrape-it/bootstrap.jsx
+++ src/scrape-it/bootstrap.jsx
@@ -3,5 +3,6 @@ import {createRoot} from 'react-dom/client'
 import ScrapeItApp from './ScrapeItApp.jsx'
 import './scrape-it.css'
 import './dealer-intel.css'
+import './control-center.css'
 
 createRoot(document.getElementById('root')).render(<ScrapeItApp/>)
