5b2f698cb20c90ebcd2925400a918bb04a8fc1f9
Rickeyjgreen <rickeyj@actionshots.online>
2026-09-13 05:52:24 -0400

    Mount scraper control center only

--- src/scrape-it/bootstrap.jsx
+++ src/scrape-it/bootstrap.jsx
@@ -1,8 +1,7 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import ScrapeItApp from './ScrapeItApp.jsx';
-import VehicleInventoryPanel from './VehicleInventoryPanel.jsx';
-import DealerPriorityPanel from './DealerPriorityPanel.jsx';
-import './scrape-it.css';
+import React from 'react'
+import {createRoot} from 'react-dom/client'
+import ScrapeItApp from './ScrapeItApp.jsx'
+import './scrape-it.css'
+import './dealer-intel.css'
 
-createRoot(document.getElementById('root')).render(<><ScrapeItApp /><div className="scrape-it-shell"><main className="si-main"><VehicleInventoryPanel /><DealerPriorityPanel /></main></div></>);
+createRoot(document.getElementById('root')).render(<ScrapeItApp/>)
